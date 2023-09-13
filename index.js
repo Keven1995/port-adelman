1d9431da8f0c2297d6646647b99a7d1df9e0551a
Keven Lucas <lkeven77@gmail.com>
2023-09-13 15:42:25 -0300

    add new file

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+
+const menuHamburger = document.querySelector(".menu-hamburger");
+const nav = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar-links li a");
+
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", "swiper-button-next", "swiper-button-prev");
+let header = document.getElementById('header');
+
+function createSwiper(container, pagination, nextButton, prevButton){
+    return new Swiper(container, {
+        slidesPerView: handleWidth(),
+        spaceBetween: 30,
+        pagination: {
+            el: pagination,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: nextButton,
+            prevEl: prevButton,
+        },
+    });
+}
+
+function handleWidth() {
+    let getWidth = window.innerWidth || document.documentElement.clientWidth;
+    let slideShow = 3;
+
+    if (getWidth < 1001) {
+        slideShow = 2;
+    }
+
+    if (getWidth < 700) {
+        slideShow = 1;
+    }
+
+    return slideShow
+}
+
+menuHamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
 });
 
-let header = document.getElementById('header')
+links.forEach(item => {
+    item.addEventListener("click", () =>{
+        nav.classList.toggle("active");
+    })
+})
+
+function openWhatsApp() {
+
+    var phoneNumber = "+556199232-5294";
+    var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber;
+    window.open(whatsappURL);
+}
+
+window.addEventListener("resize", () => {
+    swiper.params.slidesPerView = handleWidth();
+    swiper.update();
+})
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 200) {
@@ -20,10 +65,3 @@ window.addEventListener('scroll', () => {
         header.style.background = 'transparent'
     }
 })
-
-function openWhatsApp() {
-    
-    var phoneNumber = "+556199232-5294";
-    var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber;
-    window.open(whatsappURL);
-}
